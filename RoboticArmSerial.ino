a964bdb7a5198f46be5fda494d44c45ac09c2f21
albsjalbs <151546010+albsjalbs@users.noreply.github.com>
2024-09-26 12:42:31 -0400

    Create RoboticArmSerial.ino
    This code takes serial input and translates it into the servo motors in the hands so that it can move!

---
+++ RoboticArmSerial.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+#define numOfValsRec 5
+#define digitsPerValRec 1
+
+Servo servoThumb;
+Servo servoIndex;
+Servo servoMiddle;
+Servo servoRing;
+Servo servoPinky;
+
+
+int valsRec[numOfValsRec];
+int stringLength = numOfValsRec * digitsPerValRec + 1;
+int counter = 0;
+bool counterStart = false;
+String receivedString;
+
+
+void setup() {
+  Serial.begin(9600);
+  servoThumb.attach(7);
+  servoIndex.attach(3);
+  servoMiddle.attach(5);
+  servoRing.attach(11);
+  servoPinky.attach(10);
+}
+void receiveData() {
+  while (Serial.available()) {
+    char c = Serial.read();
+
+    if (c == '$') {
+      counterStart = true;
+    }
+    if (counterStart) {
+      if (counter < stringLength) {
+        receivedString = String(receivedString + c);
+        counter++;
+      }
+      if (counter >= stringLength) {
+        for (int i = 0; i < numOfValsRec; i++) {
+          int num = (i * digitsPerValRec) + 1;
+          valsRec[i] = receivedString.substring(num, num + digitsPerValRec).toInt();
+        }
+        receivedString = "";
+        counter = 0;
+        counterStart = false;
+      }
+    }
+  }
+}
